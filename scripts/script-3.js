81ce50ee0a98930762806cc6b5dc92783c5edfbc
Kim Moberg <kimskogfelter@outlook.com>
2025-01-13 11:44:09 +0100

    math random then

--- scripts/script-3.js
+++ scripts/script-3.js
@@ -59,26 +59,59 @@ function dice() {
         console.log("Tärningen visar...");
 
         // simulera...
-        setInterval(() => {
+
+        // spara id så att intervallet kan avbrytas 
+        let id = setInterval(() => {
+
+            // slumpat värde för antal graders rotation...
+            const r = Math.floor(Math.random() * 360);
 
             // påverka utseendet på elementet spanDice
+            spanDice.style.transform = `rotateZ(${r}deg)`;
+            // console.log("tick");
 
+            // hur kan man sen få värdet i span elementet att påverkas?
+            // ett utav talen 123456 - en array ....
+            const numbers = [1,2,3,4,5,6];
+
+            // ett av talen kan fås genom att ange index: positionen 
+            // ex så ger följande värdet 2 eftersom en array är 0 index baserad
+            // numbers[1] 
+            spanDice.textContent = numbers[Math.floor(Math.random() * 6)];
         }, 1000 / 60);
 
         setTimeout(() => {
-            resolve(2);
-        }, Math.random() * 3000);
+            
+            // svaret ska ett heltal mellan 1  och 6 
+            // stegvis...
+            // ett slumptal mellan 0 och 1
+            // let randomNumber = Math.random(); 
+
+            // omvandla talet så att det kan avrundas till ett heltal mellan 1 och 6
+            // randomNumber = randomNumber * 6;
+
+            // avrunda uppåt till närmsta heltal med Math.ceil()
+            // resolve(Math.ceil(randomNumber));
+            // resolve(2);
+
+            // som en enrads kod...
+            const value = Math.ceil(Math.random() * 6);
+            resolve(value);
+
+            // avbryt intervallet
+            clearInterval(id);
+
+            // manuellt ang vilket värdet blev så att det överensstämmer med
+            // tärningen
+            spanDice.textContent = value;
+
+
+        }, Math.random() * 1000);
         
     })
 }
 
+// en promise baserad funktion hanteras med nyckelordet 'then'
 dice().then((value) => {
     console.log(`... ${value}`);
 });
-
-// Uppgift: istället för att funktionen dice ger ett resultat efter en bestämd tid
-// ... ange ett slumvärde: 2000ms ... 1432ms ... 405ms ... 5703ms ...
-
-// simulera att tärningen spinner i väntan på ett resultat
-
-// Uppgift: fortsätt koda tärningen som snurrar
